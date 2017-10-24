d9658085a7fbc8c89125384f50b4b94165b21fa8
Robstepa <zb.arthur@gmail.com>
2017-10-24 21:11:40 +0200

    Add add_task function

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ from task_list import TaskList
 from os import system
 
 
+def add_task(todo_list):
+    while True:
+        name = input("Enter your name(max 20 symbols): ")
+        if len(name) > 20:
+            print("Your name is too long")
+            continue
+        description = input("Describe your task(max 150 symbols): ")
+        if len(description) > 150:
+            print("Your description is too long")
+            continue
+        todo_task = Task(name, description)
+        todo_list.add_task(name, description)
+        break
+
+
 def main():
     system('clear')
     todo_list = TaskList()
