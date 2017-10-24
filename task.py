834c88fb41980082131d8c0b57a29e9af1f02b0d
Robstepa <zb.arthur@gmail.com>
2017-10-24 18:50:13 +0200

    Create __init__ and change_task_status methods in Task class

---
+++ task.py
@@ -0,0 +1,9 @@
+class Task():
+    is_done = False
+
+    def __init__(self, name, description):
+        self.name = name
+        self. description = description
+
+    def change_task_status(self):
+        self.is_done = True
